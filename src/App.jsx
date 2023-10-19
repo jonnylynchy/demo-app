e005dacd517fd1729fe663e2aa25f2e61dfbc20d
Jon Lynch <jonnylynchy@gmail.com>
2023-10-18 21:26:49 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Dodo from './Dodo';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Dodo message="Hey, I'm a big DODO!" />
+    </>
+  )
+}
+
+export default App
---
+++ src/Dodo.jsx
@@ -0,0 +1,9 @@
+function Dodo(props) {
+
+
+    return (
+        <div>{ props.message }</div>
+    );
+}
+
+export default Dodo;
